ccd3e94c048cf77a89001117a2e5f088bd34867b
mwang840 <90732327+mwang840@users.noreply.github.com>
2021-12-06 22:24:04 -0500

    Update HashMap.cpp

--- HashMap.cpp
+++ HashMap.cpp
@@ -1,70 +1,289 @@
-
-
+#include <bits/stdc++.h>
 #include "hashMap.hpp"
+#include "hashNode.hpp"
 #include <iostream>
-#include <cstdlib>
-#include <time.h>
-
+#include <string.h>
+#include <math.h>
 using namespace std;
 
+//Constructor for the hashmap. //when creating the
+//map, make sure you initialize the values to
+//NULL so you know whether that index has a key
+//in it or is set to NULL
+// ****here you’re just looping through the map of hashNodes (a pointer to an array of
+//pointers to hashNodes) and initializing the array to NULL
+
+hashMap::hashMap(bool hash1, bool coll1) {
+	hashfn = hash1;
+	collfn = coll1;
+	numKeys = 0;
+	hashcoll = 0;
+	collisions = 0;
+	mapSize = primes[0];
+	map = new hashNode*[mapSize];
+	for(int i = 0; i < mapSize; ++i){
+		map[i] = NULL;
+	}
+}
+
+//
+int hashMap::hashFunc(string k){
+	return k.length() % mapSize;
+}
+
+//Adds the string (v) while checking to see if the key is empty or not
+void hashMap::addKeyValue(string k, string v) {
+	int index = 0;
+	//Checks if there are any hash functions to be called
+	if(hashfn == true){
+		index = calcHash1(k);
+	}
+	else{
+		index = calcHash2(k);
+	}
+	//Check to see if the index is null
+	if(map[index] == NULL){
+		cout<<"The index is null thus we can put it on the map"<<endl;
+		map[index] = new hashNode(k,v);
+		numKeys++;
+	}
+	else if(map[index]->keyword == k){
+		map[index]->addValue(v);
+		cout<<"The keyword is found"<<endl;
+	}
+	//Calls collisions and checks the collision functions
+	else{
+		hashcoll++;
+		if(collfn == true){
+			index = coll1(index, hashcoll, k);
+			cout<<"The collision happened at "<<index<<endl;
+			//Checks to see if index is empty
+			if(map[index] == NULL){
+				map[index] = new hashNode(k,v);
+				numKeys++;
+			}
+			//Checks to see if the keyword is found
+			else if(map[index]->keyword ==k){
+				map[index]->addValue(v);
+			}
+		}
+		else{
+			index = coll2(index, hashcoll, k);
+			//Checks to see if index is empty
+			if(map[index] == NULL){
+				cout<<"Its empty"<<endl;
+				map[index] = new hashNode(k,v);
+				numKeys++;
+			}
+			//Checks to see if the keyword is found
+			else if(map[index]->keyword == k){
+				cout<<"Found a match"<<endl;
+				map[index]->addValue(v);
+			}
+		}
 
 
-hashNode::hashNode(string s){
-	keyword = s;
-	values = new string[100];
-	valuesSize = 100;
-	currSize = 0;
-	srand(time(NULL));
+	}
+
 }
-hashNode::hashNode(){
-	keyword = "";
-	values = new string[100];
-	valuesSize = 100;
-	currSize = 0;
-	srand(time(NULL));
-}
-hashNode::hashNode(string s, string v){
-	keyword = s;
-	values = new string[100];
-	values[0] = v;
-	valuesSize = 100;
-	currSize = 1;
-}
-void hashNode::addValue(string v) {
-	// adding a value to the end of the value array associated
-	// with a key
-	//The current size is updated as well
-	values[currSize++] = v;
-}
-void hashNode::dblArray() {
-	// when the value array gets full, you need to make a new 
-	// array twice the size of the old one (just double, no 
-	//going to next prime) and then copy over the old values 
-	//to the new values, then de-allocate the old array.  
-	//Again, just copying over, no hash function involved
-	//here.
-	if(currSize == valuesSize){
-		valuesSize = valuesSize * 2;
-		string *newValues = values;
-		newValues = new string[valuesSize];
-		for(int i = 0; i < valuesSize; ++i){
-			newValues[i] = i;
-			newValues[i] = values[i];
+
+//Finds the key in the hashmap. No loops or 50% grade deduction. Besides, finding in hashing is O(1) not O(n) ya noob!
+int hashMap::getIndex(string k) {
+	cout<<"Testing getIndex and also calling functions inside it too"<<endl;
+	int key = 0;
+	if(hashfn == true){
+		key = calcHash1(k);
+	}
+	else if(hashfn == false){
+		key = calcHash2(k);
+	}
+
+	if(map[key] != NULL && map[key]->keyword == k){
+		return key;
+	}
+	else{
+		if(collfn == true){
+			int collide = coll1(key, 0, k);
+			cout<<"index found at :" << collide<<endl;
+			cout<<"key found at :" << map[collide]->keyword<<endl;
+			return collide;
+		}
+		else{
+			int collide = coll2(key, 0, k);
+			cout<<"index found at :" << collide<<endl;
+			cout<<"key found at :" << map[collide]->keyword<<endl;
+			return collide;
 		}
 	}
-	delete []values;
+}
+
+//This function also does linear probing but to the ith power.
+int hashMap::calcHash2(string k) {
+	cout << "calcHash2" << endl;
+	unsigned long int hash = 0;
+	for (int i = 0; i < k.length(); i++) {
+		hash += pow(k[i], i);
+	}
+	return hash % mapSize;
+}
+
+
+//Goes over each character, and performs quadratic probing on the characters with 2^i.
+int hashMap::calcHash1(string k) {
+	cout << "calcHash1" << endl;
+	unsigned long int hash = 0;
+	for (int i = 0; i < k.length(); i++) {
+		hash += k[i] * pow(2, i);
+	}
+	cout << "calcHash1 end k: "<<k<<endl;
+	cout << "hash%mapSize: " << hash%mapSize<<endl;
+	return hash % mapSize;
+}
+
+//I opted to do binary search
+void hashMap::getClosestPrime() {
+	cout<<"Testing getClosestPrime()"<<endl;
+	int start = 0;
+	int replacedSize = mapSize * 2;
+	int end = sizeof(primes);
+
+	//Binary search from the primes
+	while(start <= end){
+		int middle = start + (start + end) / 2;
+		if(primes[middle] > replacedSize && primes[middle - 1] < replacedSize){
+			mapSize = primes[middle];
+			break;
+		}
+		//If middle index < doubledSize, increment start by 1
+		if(primes[middle] < replacedSize){
+			start = middle + 1;
+		}
+		//If middle index > doubledSize, increment end by 1
+		else{
+			end = middle - 1;
+		}
+	}
+
+	map = new hashNode*[mapSize];
+}
+
+//Rehashes the array via copying the old values in the new data then deleting the old value
+//1) Create new hashmap double the size
+//2) Collect nodes of current hashmap
+//3)Place collected nodes in newhashmap
+//4)Set map global variable to newHashmap
+
+void hashMap::reHash() {
+	cout<<"Testing rehash"<<endl;
+	int existSize = mapSize;
+	//Integer to access the index and the string, target
+	int index;
+
+	getClosestPrime();
+	hashNode **old = map;
+	hashNode **temp = map;
+	string target;
+	for(int i = 0; i < existSize; ++i){
+		if(old[i] != NULL){
+			target = old[i]->keyword;
+			index = getIndex(target);
+			map[index] = old[i];
+		}
+
+	}
+	delete old;
+	int hashing;
+	for(int i = 0; i < existSize; ++i){
+		if(&temp[i] == NULL){
+			continue;
+			hashing = 0;
+		}
+		if(hashfn == true){
+			hashing = calcHash1(temp[i]->keyword);
+			if(map[hashing] != NULL){
+					if(collfn == true){
+						hashing = coll1(hashing, 0, temp[i]->keyword);
+					}
+					else{
+						hashing = coll2(hashing, 0, temp[i]->keyword);
+					}
+			}
+			else{
+				hashing = calcHash2(temp[i]->keyword);
+				if(map[hashing] != NULL){
+					if(collfn == true){
+						hashing = coll1(hashing, 0, temp[i]->keyword);
+					}
+					else{
+						hashing = coll2(hashing, 0, temp[i]->keyword);
+						}
+				}
+			}
+		}
+
+	}
 
 }
 
-string hashNode::getRandValue() {
-	//Every key has a values array - an array of words that 
-	// follow that key in the text document.  You're going to 
-	//randomly select one of those words and return it.  That 
-	//will be the word that follows your key in your output 
-	//function, and it will also be the next key.
-	string random = "";
-	int mixed = rand() % valuesSize;
-	random = values[mixed];
-	return random;
+//Probing starts here, this one is recursion and does chaining
+int hashMap::coll1(int h, int i, string k) {
+	cout << "Testing coll1" << endl;
+	h++;
+	if (h >= mapSize) {
+		//cout << "if1"<<endl;
+		h = 0;
+	}
+	if (map[h] == NULL) {
+		//cout << "if2"<<endl;
+		cout << "coll1 ret Null: " << h << endl;
+		return h;
+	} else if (map[h]->keyword == k) {
+		//cout << "if3"<<endl;
+		cout << "coll1 ret Mathc: " << h << endl;
+		return h;
+	} else {
+		//cout << "if4"<<endl;
+		return coll1(h, i, k);
+	}
+	//cout<<"end"<<endl;
+	cout << "coll1 ret: " << h << endl;
+	return h;
+
 
 }
+//Other methods for probing collisions, in this case quadratic probing is used
+int hashMap::coll2(int h, int i, string k) {
+	cout << "Testing coll2" << endl;
+	h = h + pow(i, 2);
+	if (h >= mapSize) {
+		h = h%mapSize;
+	}
+	if (map[h] == NULL) {
+		return h;
+	} else if (map[h]->keyword == k) {
+		return h;
+	} else {
+		i++;
+		return coll2(h, i, k);
+	}
+	return h;
+}
+
+
+
+//Prints out the map
+void hashMap::printMap() {
+	cout << "In printMap()" << endl;
+	for (int i = 0; i < mapSize; i++) {
+		//cout << "In loop" << endl;
+		if (map[i] != NULL) {
+			cout << map[i]->keyword << ": ";
+			for (int j = 0; j < map[i]->currSize;j++) {
+				cout << map[i]->values[j] << ", ";
+			}
+			cout << endl;
+		}
+	}
+}
+
+
